45d9bfda0c79d74822498c271a74efc76719a4f5
chandana-web <143087553+chandana-web@users.noreply.github.com>
2023-10-24 13:24:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ for (let i = 0; i<touristSpots.length ; i++) {
 str=str.replace(/\ba\b|\ban\b|\bthe\b/gi, "");
 	str=str.replace(/ +/gi, " ");
 	str=str.trim();
-	map[str]=arr[i];
+	map[str]=touristSpots[i];
 	wiart.push(str);
 }
 wiart.sort();
