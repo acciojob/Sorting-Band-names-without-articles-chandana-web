ea2ef450289c3e8b45257a9cee64297cd25deff4
chandana-web <143087553+chandana-web@users.noreply.github.com>
2023-10-26 18:52:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 //your code here
 let arr = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-let withoutArticles=[];
-let map={};
-for(let i = 0; i<arr.length ; i++) {
-	let str= arr[i];
-	str=str.replace(/\ba\b|\ban\b|\bthe\b/gi, "");
-	str=str.replace(/ +/gi, " ");
-	str=str.trim();
-	
-	withoutArticles.push(str);
+let withoutArticles = [];
+let map = {};
+
+for (let i = 0; i < arr.length; i++) {
+    let str = arr[i];
+
+
+    str = str.replace(/\b(?:a|an|the)\b/gi, ''); 
+
+    str = str.replace(/\s+/gi, ' '); 
+    str = str.trim();
+    map[str] = arr[i];
+    withoutArticles.push(str);
 }
+
 withoutArticles.sort();
-for(let i=0;i<withoutArticles.length;i++){
-	console.log(map[withoutArticles[i]]);
-	
+
+for (let i = 0; i < withoutArticles.length; i++) {
+    console.log(map[withoutArticles[i]);
 }
+
